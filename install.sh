beae1b816babbd60ec6ee619cf8ba7bbaf680f19
Harrison Breen <hdbreen@gmail.com>
2022-05-04 04:25:30 +1000

    Added Cleanup Line to Install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ sudo apt-get install ffmpeg -y
 sudo pip install pytz
 sudo apt-get install apache2 -y
 sudo apt-get install php7.4 libapache2-mod-php7.4 php7.4-mbstring php7.4-mysql php7.4-curl php7.4-gd php7.4-zip -y
-sudo service apache2 restart
+sudo service apache2 restartsufo
 sudo apt-get install -y systemd
+sudo rm -rf /etc/systemd/system/core.service
 sudo mv core.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable core.service
